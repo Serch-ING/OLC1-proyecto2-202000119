5e87c136ab6f84f35911d54f19c6476cf1cfc057
Serch-ING <3000523830101@ingenieria.usac.edu.gt>
2022-04-03 21:47:46 -0600

    agregando server

---
+++ server/app.js
@@ -0,0 +1,33 @@
+var express  = require('express');
+var morgan =  require('morgan');
+var app= express ();
+
+app.use(morgan( 'dev'));
+app.use(express.json())
+app.use(express.urlencoded ({extended: true}));
+
+
+var incremental =0;
+
+app. listen (8080, function (){
+    console.log("Escuchando en el puerto 8080")
+})
+
+app.get ('/', function (req, res) {
+    incremental++
+    res.json({mensaje: "hola mundo"})
+})
+
+app.get ('/getIncremental', function (req, res) {
+    res.json({Incremental: incremental})
+})
+
+app.get ('/retornoTexto', function (req, res) {
+    res.send('Este mensaje esta en texto')
+})
+
+app.post ('/setIcremental', function (req, res) {
+    incremental= req.body.dato
+    var texto= req. body.texto
+    res.json ({msg: "operacion con exito!" + texto})
+})
